8bca8c4bf589e80ee091db912c35e641fd3747bf
Sebastian Powroźnik <powrseba@gmail.com>
2019-07-28 20:32:02 +0200

    Time to set bean for stage only

--- src/main/java/com/springfx/config/FXController.java
+++ src/main/java/com/springfx/config/FXController.java
@@ -5,16 +5,17 @@ import com.springfx.config.scenes.StageManager;
 import javafx.fxml.Initializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Lazy;
 
-@Controller
 public abstract class FXController implements Initializable {
 
     @Autowired
     private ApplicationContext context;
+    @Lazy
+    @Autowired
+    private StageManager stageManager;
 
     protected void switchScene(FXScene scene) {
-        StageManager stageManager = context.getBean(StageManager.class);
         stageManager.switchScene(scene);
     }
 
